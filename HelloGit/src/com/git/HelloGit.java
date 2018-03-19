d1ce4eb311dd2053e7dc1e86a5f4adfee2e9e1c8
daiyuxing <286363837@qq.com>
2018-03-19 16:11:23 +0800

    加上输出Hello

--- HelloGit/src/com/git/HelloGit.java
+++ HelloGit/src/com/git/HelloGit.java
@@ -4,6 +4,11 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git !");
+		String a="H";
+		String b="e";
+		String c="l";
+		String d="o";
+		System.out.print(a+b+c+c+d);
 
 	}
 
