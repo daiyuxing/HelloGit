7b26f0787df8bd28e47c3e9362b5de06efab57bf
daiyuxing <286363837@qq.com>
2018-03-19 16:13:11 +0800

    更换输出顺序

--- HelloGit/src/com/git/HelloGit.java
+++ HelloGit/src/com/git/HelloGit.java
@@ -3,13 +3,12 @@ package com.git;
 public class HelloGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Git !");
 		String a="H";
 		String b="e";
 		String c="l";
 		String d="o";
 		System.out.print(a+b+c+c+d);
-
+		System.out.println("Hello Git !");
 	}
 
 }
